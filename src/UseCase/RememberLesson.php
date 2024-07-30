0afbabba889d35a6d23268ddebbf5c02f8dc4727
Jarno Vogelzang <Jarnovogelzang@live.nl>
2024-07-30 17:44:49 +0200

    Integration with SonarCloud via SonarLint plugin in PHPStorm.

--- src/UseCase/RememberLesson.php
+++ src/UseCase/RememberLesson.php
@@ -4,14 +4,12 @@ namespace Scripture\Memorization\UseCase;
 
 class RememberLesson
 {
-    public function execute(string $lesson): string
+    public function execute(string $lesson): void
     {
         file_put_contents(
             filename: __DIR__.'/../../storage/lessons.txt',
             data: $lesson.PHP_EOL,
             flags: FILE_APPEND
         );
-
-        return 'Added the lesson to your list.';
     }
 }
